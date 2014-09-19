e1608c1db81152d352698549ecf0674158118492
Serebro <sergilyin@gmail.com>
2014-09-19 22:59:49 +0300

    service container

--- src/Reach/Service/Container.php
+++ src/Reach/Service/Container.php
@@ -10,6 +10,7 @@ class Container
 
     protected static $services = [];
 
+    protected static $configs = [];
 
     public static function set($name, $service)
     {
@@ -18,11 +19,32 @@ class Container
 
     public static function has($name)
     {
-        return array_key_exists($name, self::$services);
+        return array_key_exists($name, self::$services) || array_key_exists($name, self::$configs);
     }
 
     public static function get($name)
     {
+        if (!isset(self::$services[$name])) {
+            self::$services[$name] = self::resolve($name);
+        }
+
         return self::$services[$name];
+    }
+
+    public static function register($service_name, $config)
+    {
+        if (!is_string($service_name) && !is_array($config) && !isset($config['class'])) {
+            throw new \InvalidArgumentException('Invalid argument');
+        }
+
+        self::$configs[$service_name] = $config;
+    }
+
+    public static function resolve($name)
+    {
+        $config = self::$configs[$name];
+        $class = $config['class'];
+        unset($config['class']);
+        return new $class($config);
     }
 }
--- tests/Service/ContainerTest.php
+++ tests/Service/ContainerTest.php
@@ -1,11 +1,19 @@
 <?php
 
+
 class ContainerTest extends PHPUnit_Framework_TestCase
 {
 
     public function testConstruct()
     {
-        $config = ['host' => 'localhost'];
+        $config = [
+            'class' => '\Reach\Connection\Dummy',
+            'host'  => 'localhost'
+        ];
+        \Reach\Service\Container::register('connection', $config);
+        $connection = \Reach\Service\Container::get('connection');
+        $this->assertInstanceOf('\Reach\Connection\Dummy', $connection);
+
         $connection = new \Reach\Connection\Dummy($config);
         \Reach\Service\Container::set('connection', $connection);
         $this->assertInstanceOf('\Reach\Connection\Dummy', \Reach\Service\Container::get('connection'));
